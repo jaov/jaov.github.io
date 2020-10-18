ea0b09802faab2d87b48625db662fefbcaf8f9de
jaov <jesalfochvel@gmail.com>
2020-10-18 18:54:02 -0400

    added start of image code to main.js

--- games/bce/scripts/main.js
+++ games/bce/scripts/main.js
@@ -1,2 +1,21 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+// Creating a new image
+let img = new Image();
+img.src = 'images/Green-Cap-Character';
+
+// Initializing when image is loaded
+img.onload = function() {
+    init();
+};
+
+// Selecting canvas on the document
+let canvas = document.querySelector('canvas');
+// ctx is context
+let ctx = canvas.getContext('2d');
+
+// body of init function
+function init() {
+    // TODO animation code
+}
