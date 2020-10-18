febe865179e66ec7f8be6219dedc718e32510b59
jaov <jesalfochvel@gmail.com>
2020-10-18 19:40:42 -0400

    cleaning up the code a bit

--- games/bce/scripts/main.js
+++ games/bce/scripts/main.js
@@ -10,36 +10,24 @@ img.src = 'images/Green-Cap-Character.png';
 
 // Initializing when image is loaded
 img.onload = function() {
-    init();
+    window.requestAnimationFrame(gameLoop);
 };
 
 // Selecting canvas on the document
 let canvas = document.querySelector('canvas');
-// ctx is context
+// ctx is context (we are using 2d)
 let ctx = canvas.getContext('2d');
 
 // variables to understand the calls better
-const scale = 2;
-const width = 16;
-const height = 18;
-const scaledWidth = scale * width;
-const scaledHeight = scale * height;
+const SCALE = 2;
+const WIDTH = 16;
+const HEIGHT = 18;
+const SCALED_WIDTH = scale * width;
+const SCALED_HEIGHT = scale * height;
 
 // The loop goes idle, left, idle, right.
 const cycleLoop = [0, 1, 0, 2];
 let currentLoopIndex = 0;
-
-// At the moment clears all the canvas as to not redraw,
-// I have to fix this later!
-function step() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
-      currentLoopIndex++;
-      if (currentLoopIndex >= cycleLoop.length) {
-              currentLoopIndex = 0;
-            }
-      window.requestAnimationFrame(step);
-}
 
 
 // Simplifying the drawImage call with variable names
@@ -47,7 +35,15 @@ function drawFrame(frameX, frameY, canvasX, canvasY) {
     ctx.drawImage(img, frameX * width, frameY * height, width, height,
                   canvasX, canvasY, scaledWidth, scaledHeight);
 }
+
+// Changing this for directions
+// and also for game loop logic
+const CYCLE_LOOP = [0, 1, 0, 2];
+let currentLoopIndex = 0;
+let frameCount = 0;
+let currentDirection = 0;
+
 // body of init function
-function init() {
-    window.requestAnimationFrame(step);
+function gameLoop() {
+    window.requestAnimationFrame(gameLoop);
 }
